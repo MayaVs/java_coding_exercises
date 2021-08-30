b6f0778113805a4b79d8249f3691a211534ce1f5
Maya Vasileva <maya.vassileva@gmail.com>
2021-08-30 14:13:22 +0100

    Submitting  the first 4 functions of exercise 001

--- src/main/java/com/techreturners/exercise001/Exercise001.java
+++ src/main/java/com/techreturners/exercise001/Exercise001.java
@@ -1,26 +1,29 @@
 package com.techreturners.exercise001;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 import java.util.List;
 
 public class Exercise001 {
     public String capitalizeWord(String word) {
-        // Add your code here
-        return "";
+        return Character.toUpperCase(word.charAt(0)) + word.substring(1);
     }
 
     public String generateInitials(String firstName, String lastName) {
-        // Add your code here
-        return "";
+        return firstName.charAt(0)+"."+lastName.charAt(0);
     }
 
     public double addVat(double originalPrice, double vatRate) {
-        // Add your code here
-        return 0.0;
+        double vat = originalPrice+originalPrice*vatRate/100;
+        DecimalFormat formatter = new DecimalFormat("#0.00");
+        return Double.parseDouble(formatter.format(vat));
     }
 
     public String reverse(String sentence) {
-        // Add your code here
-        return "";
+        if(sentence.length() == 1)
+            return sentence;
+        else
+            return reverse(sentence.substring(1))+sentence.charAt(0);
     }
 
     public int countLinuxUsers(List<User> users) {
